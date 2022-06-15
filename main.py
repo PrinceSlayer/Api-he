91174cd0dbad381aa08bdcac6fa5639d06ecfb99
PrinceSlayer <106213310+PrinceSlayer@users.noreply.github.com>
2022-06-14 20:15:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import *
+import os
+import aminofix
+os.system('set FLASK_ENV=development')
+app = Flask(__name__)
+
+
+@app.route('/api/login',  methods = ['POST'])
+def get_timezone():
+  data = request.form
+  login = data.get("login")
+  password = data.get("password")
+  device = data.get("device")
+  client = aminofix.Client(device)
+  client.login(email = login, password = password)
+  return f"{client.sid}"
+
+if __name__ == '__main__':
+  app.run("0.0.0.0", 5000)
