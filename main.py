6c0b4433268585376cbacbb13ebc8a3321a3e5fa
PrinceSlayer <106213310+PrinceSlayer@users.noreply.github.com>
2022-06-29 21:29:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import os
 import aminofix
 import random
 import time
+import heroku3
+
+
 os.system('set FLASK_ENV=development')
 app = Flask(__name__)
+
+def restart():
+    heroku_conn = heroku3.from_key("b0fce155-8839-436a-b0e2-e6e02699208d")
+    botapp = heroku_conn.apps()["sid-amino"]
+    botapp.restart()
+
 @app.route('/api/login',  methods = ['POST'])
 def get_timezone():
   data = request.form
@@ -29,6 +38,7 @@ def get_timezone():
           return f"{client.sid}"
         except Exception as g:
           print(f"Não foi possivel realizar o login, pelo erro: {g}")
+          restart()
 
 if __name__ == '__main__':
   app.run("0.0.0.0", random.randint(2000, 9000))
